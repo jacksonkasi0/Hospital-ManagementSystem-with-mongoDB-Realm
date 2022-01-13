847542c50e30a25f30f870af5ad57ac72c66d1c1
Jackson Kasi <68270206+jacksonkasi0@users.noreply.github.com>
2022-01-13 07:58:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
+  const MONGODB_URL="mongodb+srv://jacksonkasi:1855@jacksonkasi.fwlzq.mongodb.net/Hospital-Management-System?retryWrites=true&w=majority"
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URL);
+    const connect = await mongoose.connect(MONGODB_URL);
     console.log(`MonogoDB Connected : ${connect.connection.host}`);
   } catch (error) {
     console.log(error);
